4646189bc8973c1e4a87ffeeed366219a2e07f9b
Yuci Gou <ygou@ebi.ac.uk>
2018-06-26 14:01:52 +0100

    Add eslint

--- pubsweet-component-minio/index.js
+++ pubsweet-component-minio/index.js
@@ -3,150 +3,141 @@ const formidable = require('formidable')
 const minioClient = require('./minio-client.js')
 const uuidv1 = require('uuid/v1')
 
-const Ops = Object.freeze({"post":1, "list":2, "get":3, "delete":4})
+const Ops = Object.freeze({'post':1, 'list':2, 'get':3, 'delete':4})
 
 const extractFileExtension = filename => {
-  if (filename) {
-    return filename.split('.').pop()
-  }
+    if (filename) {
+        return filename.split('.').pop()
+    }
 
-  return ''
+    return ''
 }
 
 const validityCheck = (req, options) => {
-	if (!options) {
-		const error = 'Options not provided'
-		req.minio = {error}
-		console.log(error)
-		return false;
-	}
+    if (!options) {
+        const error = 'Options not provided'
+        req.minio = {error}
+        return false
+    }
 
-	let supported = false;
-	for (const property in Ops) {
-		if (Ops[property] === options.op) {
-			supported = true
-			break
-		}
-	}
-	if (!supported) {
-		const error = 'Operation not supported'
-		req.minio = {error}
-		console.log(error)
-		return false;
-	}
+    let supported = false
+    for (const property in Ops) {
+        if (Ops[property] === options.op) {
+            supported = true
+            break
+        }
+    }
+    if (!supported) {
+        const error = 'Operation not supported'
+        req.minio = {error}
+        return false
+    }
 
-	return true;
+    return true
 }
 
 const handlePost = (req, next, fields, files) => {
     let filename = uuidv1()
     const extension = extractFileExtension(files.file.name)
     if (extension) {
-    	filename += '.' + extension
+        filename += '.' + extension
     }
 
-	minioClient.uploadFile(
-		filename,
-		files.file.name,
-		files.file.type,
-		files.file.path,
-		(error, etag) => {
-			if (error) {
-				req.minio = {error}
-				console.log(error)
-			} else {
-				req.minio = {post: { filename: `${filename}` }}
-			}
-			next()
-			return
-		},
-	)
+    minioClient.uploadFile(
+        filename,
+        files.file.name,
+        files.file.type,
+        files.file.path,
+        (error, etag) => {
+            if (error) {
+                req.minio = {error}
+            } else {
+                req.minio = {post: { filename: `${filename}`, etag }}
+            }
+            next()
+            return
+        }
+    )
 }
 
-const handleList = (req, next, fields, files) => {
-	minioClient.listFiles(
-		(error, list) => {
-			if (error) {
-				req.minio = {error}
-				console.log(error)
-			} else {
-				req.minio = {list}
-			}
-			next()
-		},
-	)
+const handleList = (req, next) => {
+    minioClient.listFiles(
+        (error, list) => {
+            if (error) {
+                req.minio = {error}
+            } else {
+                req.minio = {list}
+            }
+            next()
+        }
+    )
 }
 
-const handleGet = (req, next, fields, files) => {
-	const tmpFile = `/tmp/${req.params.filename}`
-	minioClient.getFile(
-		req.params.filename,
-		tmpFile,
-		error => {
-			if (error) {
-				req.minio = {error}
-				console.log(error)
-			} else {
-				req.minio = {get: tmpFile}
-			}
-			next()
-		},
-	)
-}
-
-const handleDelete = (req, next, fields, files) => {
-	minioClient.deleteFile(
+const handleGet = (req, next) => {
+    const tmpFile = `/tmp/${req.params.filename}`
+    minioClient.getFile(
         req.params.filename,
-		error => {
-			if (error) {
-				req.minio = {error}
-				console.log(error)
-			} else {
-				req.minio = {delete: 'Success'}
-			}
-			next()
-		},
+        tmpFile,
+        error => {
+            if (error) {
+                req.minio = {error}
+            } else {
+                req.minio = {get: tmpFile}
+            }
+            next()
+        }
+    )
+}
+
+const handleDelete = (req, next) => {
+    minioClient.deleteFile(
+        req.params.filename,
+        error => {
+            if (error) {
+                req.minio = {error}
+            } else {
+                req.minio = {delete: 'Success'}
+            }
+            next()
+        }
     )
 }
 
 const handleRequests = (req, next, options) => {
-	if (!validityCheck(req, options)) {
-		next()
-		return;
-	}
+    if (!validityCheck(req, options)) {
+        next()
+        return
+    }
 
-	const form = new formidable.IncomingForm()
-	req.minio = {error: 'No error'}
+    const form = new formidable.IncomingForm()
+    req.minio = {error: 'No error'}
 
-	form.parse(req, (err, fields, files) => {
-	    if (err) {
-			req.minio = {error: err}
-			console.log(err)
-			next()
-			return;
-	    }
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            req.minio = {error: err}
+            next()
+            return
+        }
 
-	    if (options.op === Ops.post) {
-	    	handlePost(req, next, fields, files)
-	    } else if (options.op === Ops.list) {
-	    	handleList(req, next, fields, files)
-	    } else if (options.op === Ops.get) {
-	    	handleGet(req, next, fields, files)
-	    } else if (options.op === Ops.delete) {
-	    	handleDelete(req, next, fields, files)
-	    } else {
-	    	console.log("Should not be here");
-	    }
-	})
+        if (options.op === Ops.post) {
+            handlePost(req, next, fields, files)
+        } else if (options.op === Ops.list) {
+            handleList(req, next)
+        } else if (options.op === Ops.get) {
+            handleGet(req, next)
+        } else if (options.op === Ops.delete) {
+            handleDelete(req, next)
+        }
+    })
 }
 
 module.exports = {
-	Ops,
-	middleware() {
-		return options => {
-			return (req, res, next) => {
-				handleRequests(req, next, options);
-			}
-		}
-	}	
+    Ops,
+    middleware() {
+        return options => {
+            return (req, res, next) => {
+                handleRequests(req, next, options)
+            }
+        }
+    }    
 }
--- pubsweet-component-minio/minio-client.js
+++ pubsweet-component-minio/minio-client.js
@@ -1,59 +1,59 @@
 const Minio = require('minio')
 
 const minioClient = new Minio.Client({
-  endPoint: process.env.MINIO_ENDPOINT,
-  port: Number(process.env.MINIO_PORT),
-  secure: process.env.MINIO_SECURITY === 'true',
-  accessKey: process.env.MINIO_ACCESS_KEY,
-  secretKey: process.env.MINIO_SECRET_KEY,
+    endPoint: process.env.MINIO_ENDPOINT,
+    port: Number(process.env.MINIO_PORT),
+    secure: process.env.MINIO_SECURITY === 'true',
+    accessKey: process.env.MINIO_ACCESS_KEY,
+    secretKey: process.env.MINIO_SECRET_KEY,
 })
 
 module.exports = {
-  uploadFile(filename, oriFilename, fileType, tempFilePath, callback) {
-    const uploads = process.env.MINIO_UPLOADS_FOLDER_NAME
-    let filePath = `${uploads}/${filename}`
+    uploadFile(filename, oriFilename, fileType, tempFilePath, callback) {
+        const uploads = process.env.MINIO_UPLOADS_FOLDER_NAME
+        let filePath = `${uploads}/${filename}`
 
-    const metaData = {
-      'content-type': fileType,
-      'file-name': oriFilename,
-    }
+        const metaData = {
+            'content-type': fileType,
+            'file-name': oriFilename,
+        }
 
-    minioClient.fPutObject(
-      process.env.MINIO_BUCKET,
-      filePath,
-      tempFilePath,
-      metaData,
-      callback,
-    )
-  },
+        minioClient.fPutObject(
+            process.env.MINIO_BUCKET,
+            filePath,
+            tempFilePath,
+            metaData,
+            callback
+        )
+    },
 
-  listFiles(callback) {
-    const uploads = process.env.MINIO_UPLOADS_FOLDER_NAME
-    const prefix = `${uploads}`
-    const stream = minioClient.listObjects(process.env.MINIO_BUCKET, prefix, true)
-    const list = []
-    stream.on('data', obj => {
-      list.push(obj)
-    })
-    stream.on('error', err => {
-      callback(err)
-    })
-    stream.on('end', () => {
-      callback(null, list)
-    })
-  },
+    listFiles(callback) {
+        const uploads = process.env.MINIO_UPLOADS_FOLDER_NAME
+        const prefix = `${uploads}`
+        const stream = minioClient.listObjects(process.env.MINIO_BUCKET, prefix, true)
+        const list = []
+        stream.on('data', obj => {
+            list.push(obj)
+        })
+        stream.on('error', err => {
+            callback(err)
+        })
+        stream.on('end', () => {
+            callback(null, list)
+        })
+    },
 
-  getFile(fileName, tmpFile, callback) {
-    const uploads = process.env.MINIO_UPLOADS_FOLDER_NAME
-    const objectName = `${uploads}/${fileName}`
-    minioClient.fGetObject(process.env.MINIO_BUCKET, objectName, tmpFile, callback)
-  },
+    getFile(fileName, tmpFile, callback) {
+        const uploads = process.env.MINIO_UPLOADS_FOLDER_NAME
+        const objectName = `${uploads}/${fileName}`
+        minioClient.fGetObject(process.env.MINIO_BUCKET, objectName, tmpFile, callback)
+    },
 
-  deleteFile(fileName, callback) {
-    const uploads = process.env.MINIO_UPLOADS_FOLDER_NAME
-    const objectName = `${uploads}/${fileName}`
-    minioClient.removeObject(process.env.MINIO_BUCKET, objectName, err => {
-      callback(err)
-    })
-  },
+    deleteFile(fileName, callback) {
+        const uploads = process.env.MINIO_UPLOADS_FOLDER_NAME
+        const objectName = `${uploads}/${fileName}`
+        minioClient.removeObject(process.env.MINIO_BUCKET, objectName, err => {
+            callback(err)
+        })
+    },
 }
